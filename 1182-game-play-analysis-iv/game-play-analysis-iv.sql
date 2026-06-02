853ba42e523e219e1955ca7616708102a4d0281b
purushotam09 <142565043+purushotam09@users.noreply.github.com>
2026-06-02 18:38:02 +0530

    Time: 1254 ms (73.79%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -13,3 +13,20 @@ LEFT JOIN Activity a
 ON f.player_id = a.player_id
 AND a.event_date = DATEADD(day, 1, f.first_login_date);
 
+/*
+select player_id, min(event_date) as first_login
+into #temp
+from Activity
+group by player_id;
+
+declare @var1 INT, @var2 INT;
+
+select @var1 = count(*) from #temp;
+
+select @var2 = count(*)
+from Activity a
+inner join #temp t
+on a.player_id = t.player_id
+and a.event_date = dateadd(day, 1, t.first_login);
+
+select round(1.0 * @var2 / @var1, 2) as fraction;*/

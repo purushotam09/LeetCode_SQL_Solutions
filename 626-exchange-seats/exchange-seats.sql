1093698d6e685bf26469b9094a9dcb44458de28d
purushotam09 <142565043+purushotam09@users.noreply.github.com>
2026-06-05 16:08:37 +0530

    Time: 484 ms (8.75%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,2 +1,12 @@
 /* Write your T-SQL query statement below */
 with cte as (select id,student,lead(id,1,0) over(order by id) as leadid from Seat) select case when id%2!=0 and leadid!=0 then id+1 when id%2!=0 and leadid=0 then id else id-1 end as id,student from cte order by id;
+
+/*SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;*/

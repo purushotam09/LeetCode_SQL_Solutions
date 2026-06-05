ceaff6126552171f82ab7085b77961b3d06b037c
purushotam09 <142565043+purushotam09@users.noreply.github.com>
2026-06-05 16:30:54 +0530

    Time: 1331 ms (52.27%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,3 +1,9 @@
-/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below 
 select distinct customer_id from Customer except 
-select distinct customer_id from ((select distinct c.customer_id , p.product_key from Customer c cross join Product p) except (select distinct customer_id,product_key from Customer)) as t;
+select distinct customer_id from ((select distinct c.customer_id , p.product_key from Customer c cross join Product p) except (select distinct customer_id,product_key from Customer)) as t;*/
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (
+    SELECT COUNT(*) FROM Product
+);

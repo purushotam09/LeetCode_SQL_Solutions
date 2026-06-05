4414d166dc11acda4f5756e86f85007f80e2aa8a
purushotam09 <142565043+purushotam09@users.noreply.github.com>
2026-06-05 16:28:30 +0530

    Time: 1950 ms (5.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,3 @@
+/* Write your T-SQL query statement below */
+select distinct customer_id from Customer except 
+select distinct customer_id from ((select distinct c.customer_id , p.product_key from Customer c cross join Product p) except (select distinct customer_id,product_key from Customer)) as t;

0963a71da0b0bba58ca6c44911c954c635fd7616
purushotam09 <142565043+purushotam09@users.noreply.github.com>
2026-06-08 16:58:34 +0530

    Time: 358 ms (72.06%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,3 @@
-/*select (select top 1 salary from Employee where salary < (select max(salary) from Employee) order by salary desc) as SecondHighestSalary */
+select (select top 1 salary from Employee where salary < (select max(salary) from Employee) order by salary desc) as SecondHighestSalary ;
 
-select (select distinct salary from Employee order by salary desc offset 1 rows fetch next 1 rows only) as SecondHighestSalary;
+/*select (select distinct salary from Employee order by salary desc offset 1 rows fetch next 1 rows only) as SecondHighestSalary*/;

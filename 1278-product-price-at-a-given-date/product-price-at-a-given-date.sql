f5ca11cef4d7c2eaa5a814c45241ca7e4e8bcf81
purushotam09 <142565043+purushotam09@users.noreply.github.com>
2026-06-08 19:53:30 +0530

    Time: 1003 ms (25.74%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -5,3 +5,26 @@ with cte as (
 select product_id,10 from Products where product_id not in (select product_id from cte)
 
 /*One more safer habit: use NOT EXISTS instead of NOT IN when NULL may exist.But here product_id is part of primary key, so NOT IN is fine.*/
+
+/*
+WITH cte AS
+(
+    SELECT *,
+           ROW_NUMBER() OVER(
+               PARTITION BY product_id
+               ORDER BY change_date DESC
+           ) AS rn
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
+SELECT product_id, new_price AS price
+FROM cte
+WHERE rn = 1
+
+UNION
+
+SELECT product_id, 10 AS price
+FROM Products
+GROUP BY product_id
+HAVING MIN(change_date) > '2019-08-16';
+*/

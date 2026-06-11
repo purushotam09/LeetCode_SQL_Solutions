4e0f57770fbb513d30ebe618758fc58efc86086b
purushotam09 <142565043+purushotam09@users.noreply.github.com>
2026-06-11 13:07:10 +0530

    Time: 457 ms (61.41%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,24 +1,4 @@
 /* Write your T-SQL query statement below */
-SELECT results
-FROM (
-    SELECT TOP 1 u.name AS results
-    FROM MovieRating mr
-    INNER JOIN Users u
-        ON mr.user_id = u.user_id
-    GROUP BY mr.user_id, u.name
-    ORDER BY COUNT(*) DESC, u.name ASC
-) AS t1
-
-UNION ALL
-
-SELECT results
-FROM (
-    SELECT TOP 1 m.title AS results
-    FROM MovieRating mr
-    INNER JOIN Movies m
-        ON mr.movie_id = m.movie_id
-    WHERE mr.created_at >= '2020-02-01'
-      AND mr.created_at < '2020-03-01'
-    GROUP BY mr.movie_id, m.title
-    ORDER BY AVG(mr.rating * 1.0) DESC, m.title ASC
-) AS t2;
+select t1.results as results from (select top 1 u.name as results from MovieRating mr inner join Users u on mr.user_id = u.user_id group by mr.user_id,u.name order by count(*) desc,u.name asc) as t1
+union all
+select t2.results as results from (select top 1 m.title as results from MovieRating mr inner join Movies m on mr.movie_id = m.movie_id and mr.created_at >= '2020-02-01' and mr.created_at < '2020-03-01' group by mr.movie_id,m.title order by avg(mr.rating*1.0) desc,m.title asc) as t2;
